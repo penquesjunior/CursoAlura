e42492e4931b8eb63695273ee6a127eee3325c42
Junior <penquesjunior@gmail.com>
2024-01-30 16:36:33 -0300

    Exceptions

--- src/br/com/trabalho/calculadora/models/Titulo.java
+++ src/br/com/trabalho/calculadora/models/Titulo.java
@@ -1,11 +1,11 @@
 package br.com.trabalho.calculadora.models;
 
+import br.com.trabalho.calculadora.excecao.ErroDeConversaoDeAnoException;
 import com.google.gson.annotations.SerializedName;
 
 public class Titulo implements Comparable<Titulo>{
-    @SerializedName("Title")
+
     private String nome;
-    @SerializedName("Year")
     private int anoDeLancamento;
     private boolean incluidoNoPlano;
     private double somaDasAvaliacoes;
@@ -21,8 +21,14 @@ public class Titulo implements Comparable<Titulo>{
 
     public Titulo(TituloOmdb meuTituloOmdb) {
         this.nome = meuTituloOmdb.title();
+        if(meuTituloOmdb.year().length()>4) {
+            throw new ErroDeConversaoDeAnoException("Não consegui converter o ano "+
+                    "Porque tem mais de 4 caracteres");
+        }
+
         this.anoDeLancamento = Integer.valueOf(meuTituloOmdb.year());
-        this.duracaoEmMinutos = Integer.valueOf(meuTituloOmdb.runtime().substring(0,2));
+
+        this.duracaoEmMinutos = Integer.valueOf(meuTituloOmdb.runtime().replace(" min",""));
     }
 
     public void exibeFichaTecnica(){
--- src/br/com/trabalho/calculadora/principal/PrincipalComBusca.java
+++ src/br/com/trabalho/calculadora/principal/PrincipalComBusca.java
@@ -1,5 +1,6 @@
 package br.com.trabalho.calculadora.principal;
 
+import br.com.trabalho.calculadora.excecao.ErroDeConversaoDeAnoException;
 import br.com.trabalho.calculadora.models.Titulo;
 import br.com.trabalho.calculadora.models.TituloOmdb;
 import com.google.gson.FieldNamingPolicy;
@@ -19,29 +20,39 @@ public class PrincipalComBusca {
         Scanner leitura = new Scanner(System.in);
         System.out.println("Digite um filme para busca:");
         var busca = leitura.nextLine();
+        try{
+            String endereco = "http://www.omdbapi.com/?t="+
+                    busca.replace(" ","+")+"&apikey=9ace215f";
 
-        String endereco = "http://www.omdbapi.com/?t="+busca+"&apikey=9ace215f";
+            HttpClient client = HttpClient.newHttpClient();
+            HttpRequest request = HttpRequest.newBuilder()
+                    .uri(URI.create(endereco))
+                    .build();
+            HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
 
-        HttpClient client = HttpClient.newHttpClient();
-        HttpRequest request = HttpRequest.newBuilder()
-                .uri(URI.create(endereco))
-                .build();
-        HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+            String json = response.body();
+            System.out.println(json);
 
-        String json = response.body();
-        System.out.println(json);
+            // pega o nome com letra minuscula para consumir api
+            Gson gson = new GsonBuilder()
+                    .setFieldNamingPolicy(FieldNamingPolicy.UPPER_CAMEL_CASE)
+                    .create();
 
-        // pega o nome com letra minuscula para consumir api
-        Gson gson = new GsonBuilder()
-                .setFieldNamingPolicy(FieldNamingPolicy.UPPER_CAMEL_CASE)
-                .create();
+            TituloOmdb meuTituloOmdb = gson.fromJson(json, TituloOmdb.class);
+            System.out.println(meuTituloOmdb);
 
-        TituloOmdb meuTituloOmdb = gson.fromJson(json, TituloOmdb.class);
-        System.out.println(meuTituloOmdb);
-
-        Titulo meuTitulo = new Titulo(meuTituloOmdb);
-        System.out.println("-----------------");
-        System.out.println("Meu titulo convertido: \n"+ meuTitulo);
-
+            System.out.println(endereco);
+            Titulo meuTitulo = new Titulo(meuTituloOmdb);
+            System.out.println("-----------------");
+            System.out.println("Meu titulo convertido: \n"+ meuTitulo);
+        }catch (NumberFormatException e){
+            System.out.println("Aconteceu um erro: ");
+            System.out.println(e.getMessage());
+        }catch (IllegalArgumentException e){
+            System.out.println("Erro de Argumento");
+            System.out.println(e.getMessage());
+        }catch (ErroDeConversaoDeAnoException e){
+            System.out.println(e.getMessage());
+        }
     }
 }
